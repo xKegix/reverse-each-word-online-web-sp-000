80e64ddf1c82aec4eaf661d8313089f100261062
Keginold Philip <keginoldp@hotmail.com>
2021-01-25 14:27:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-1. Write method called reverse_each_word.
-2. Take in string argument and returns same sentence with each word reversed in place.
+# 1. Write method called reverse_each_word.
+# 2. Take in string argument and returns same sentence with each word reversed in place.
 
 
-def reverse_each_word
+def reverse_each_word(string)
   reverse_each_word.each do |word|
     reverse_each_word.reverse!
   end
