2425e99b4b7df65b54b1206a4e2b8fc8eb96303c
Keginold Philip <keginoldp@hotmail.com>
2021-01-25 14:27:15 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+1. Write method called reverse_each_word. 
+2. Take in string argument and returns same sentence with each word reversed in place.
+
+
+def reverse_each_word
+  reverse_each_word.each do |word|
+    reverse_each_word.reverse!
+  end
+end
