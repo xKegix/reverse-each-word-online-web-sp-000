433684ac76dca8e41f4b5fd9f6a09f96a8d010dd
Keginold Philip <keginoldp@hotmail.com>
2021-01-25 14:33:08 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+# 1. Write method called reverse_each_word.
+# 2. Take in string argument and returns same sentence with each word reversed in place.
+
+
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
+end
